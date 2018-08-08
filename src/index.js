ae727e10f897f340d249d1a5e0eac80151299073
Hector Sanchez <hsanchez7934@gmail.com>
2018-08-08 12:21:04 -0600

    Display city name for user

--- src/index.js
+++ src/index.js
@@ -22,13 +22,15 @@ async function fetchData(query) {
   if (!isNaN(parseInt(query))) {
     const byZipCodeUrl = `https://api.openweathermap.org/data/2.5/forecast?zip=${query},US&APPID=${key}`;
     const response = await axios.get(byZipCodeUrl);
+    const city = response.data.city.name;
     const weatherData = parseData(response.data.list);
-    displayData(weatherData);
+    displayData(weatherData, city);
   } else if (isNaN(parseInt(query))) {
     const byCityUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${query},us&mode=json&APPID=${key}`;
     const response = await axios.get(byCityUrl);
+    const city = response.data.city.name;
     const weatherData = parseData(response.data.list);
-    displayData(weatherData);
+    displayData(weatherData, city);
   }
 }
 
@@ -43,24 +45,33 @@ function parseData(weatherData) {
   }, {});
 }
 
-function displayData(weatherData) {
+function displayCityName(city) {
+  let h1 = document.createElement('h1');
+  h1.setAttribute('id', 'city-title');
+  h1.textContent = city;
+  return h1;
+}
+
+function displayData(weatherData, city) {
+  const cityNameWrapper = document.getElementById('city-name-wrapper');
   const section = document.getElementById('bottom-main');
   const fragment = document.createDocumentFragment();
   const listWrapper = document.createElement('div');
   listWrapper.setAttribute('id', 'list-wrapper');
-
+  const cityName = displayCityName(city);
+  cityNameWrapper.appendChild(cityName);
   for (let day in weatherData) {
     let date = weatherData[day];
     let dateCard = document.createElement('article');
-    dateCard.textContent = day;
+    dateCard.textContent = 'Date: ' + day;
     listWrapper.appendChild(dateCard);
     fragment.appendChild(listWrapper);
 
     for (let time in date) {
       let timeUL = document.createElement('ul');
       let timeLI = document.createElement('li');
-      timeUL.textContent = time;
-      timeLI.textContent = date[time];
+      timeUL.textContent = 'Time: ' + time;
+      timeLI.textContent = 'Temp: ' + date[time];
       dateCard.appendChild(timeUL);
       timeUL.appendChild(timeLI);
     }
